9b5da7edeea8a05b78c66345996da76118484530
GummiCode <43494071+GummiCode@users.noreply.github.com>
2020-06-14 05:01:00 +0100

    Added basic movement functions. Event listeners are used to detect key presses of the right and left arrows, and key releases of the same arrows. Coordinates of the plyer object change accordingly. 20ms loop update.

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ const player = {
 	width: 20
 };
 
+const arrowKeys = {
+	right: false,
+	left: false,
+	up: false,
+}
+
+const friction = 0.7;
+
+const keyDown = (key) => {
+	if (key.keyCode === 39) {arrowKeys.right = true};
+	if (key.keyCode === 37) {arrowKeys.left = true};
+	//TODO: Can ternary if be used for statements with only one outcome?
+};
+
+const keyUp = (key) => {
+	if (key.keyCode === 39) {arrowKeys.right = false};
+	if (key.keyCode === 37) {arrowKeys.left = false};
+	//TODO: Can ternary if be used for statements with only one outcome?	
+}
+
 let numPlatforms = 2;
 
 let platforms = [];
@@ -40,7 +60,15 @@ const renderPlatforms = () => {
 	platforms.forEach(platform => ctx.fillRect(platform.x, platform.y, platform.width, platform.height));
 }
 
-const renderAll = () => {
+const gameLoop = () => {
+	if(arrowKeys.right) {
+		player.x += 2.5;
+	};
+
+	if(arrowKeys.left) {
+		player.x += -2.5;
+	};
+
 	renderCanvas();
 	renderPlayer();
 	renderPlatforms();
@@ -51,4 +79,6 @@ ctx = canvas.getContext("2d");
 ctx.canvas.height = 270;
 ctx.canvas.width = 270;
 createPlatforms();
-renderAll();
+document.addEventListener("keyup", keyUp);
+document.addEventListener("keydown", keyDown);
+setInterval(gameLoop, 20);
