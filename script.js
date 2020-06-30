77cacae13d0d3e40f01a759297a16b7143cdc013
GummiCode <gummicodeblog@gmail.com>
2020-06-30 17:29:47 +0100

    Refactored significantly. Organised code by app elements. Made a 'final' MVP todo list at the end of script.js.

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+//Canvas Settings /////////////////////////////////////
+
+const canvasParams = {
+	fillColor: "lightYellow",
+	origin_x: 0,
+	origin_y: 0,
+	height: 500,
+	width: 500,
+}
+
+const renderCanvas = () => {
+	ctx.fillStyle = `${canvasParams.fillColor}`;
+	ctx.fillRect(canvasParams.origin_x, canvasParams.origin_y, canvasParams.height, canvasParams.width);
+};
+
+//Physical Parameters & Constants ///////////////////////
+
+let gravity = 0.6;
+const friction = 0.7;
+
+
+// Player Settings ////////////////////////////
+
 const player = {
 	x: 25,
 	y: 10,
@@ -6,6 +29,11 @@ const player = {
 	jump: true,
 	height: 20,
 	width: 20
+};
+
+const renderPlayer = () => {
+	ctx.fillStyle = "#F08080";
+	ctx.fillRect((player.x)-20, (player.y)-20, player.width, player.height);
 };
 
 const setPlayerDefault = () => {
@@ -18,23 +46,14 @@ const setPlayerDefault = () => {
 	player.width = 20
 };
 
+//Control Settings //////////////////////////////////////////
+
 const arrowKeys = {
 	right: false,
 	left: false,
 	jump: false,
 }
 
-const canvasParams = {
-	fillColor: "lightYellow",
-	origin_x: 0,
-	origin_y: 0,
-	height: 500,
-	width: 500,
-}
-
-let gravity = 0.6;
-const friction = 0.7;
-let dodgerSpeed = 0.1;
 
 const keyDown = (key) => {
 	if (key.keyCode === 37) {arrowKeys.left = true};
@@ -56,22 +75,32 @@ const keyUp = (key) => {
 	}
 };
 
+/*TO ADD: 
+Control diagram somewhere on screen. erhaps under the canvas?
+Event listeners to change button highlight when they're held down
+*/
+
+const retryButton = document.querySelector("#retry");
+const newLevelButton = document.querySelector("#new-level");
+
+const buttonTextFlash = (buttonName) => {
+	buttonName.style.color = "#E0E0E0";
+	buttonName.style.transition = "color 0.2s linear";
+	
+	setTimeout(() => {
+		buttonName.style.color = "unset";
+		buttonName.style.transition = "color 0.2s linear";
+	}, 300);
+};
+
+//Platform & Dodger Settings /////////////////////////////////////
+
 let numPlatforms = 5;
-
-let platforms = [];
-let dodgers = [];
-
-const renderCanvas = () => {
-	ctx.fillStyle = `${canvasParams.fillColor}`;
-	ctx.fillRect(canvasParams.origin_x, canvasParams.origin_y, canvasParams.height, canvasParams.width);
-};
-
-const renderPlayer = () => {
-	ctx.fillStyle = "#F08080";
-	ctx.fillRect((player.x)-20, (player.y)-20, player.width, player.height);
-};
-
 let currentPlatformY = 250;
+let platforms = [];
+
+let dodgerSpeed = 0.1;
+let dodgers = [];
 
 const createPlatforms = () => {
 	for (i=0; i<numPlatforms; i++) {
@@ -117,30 +146,33 @@ const renderPlatforms = () => {
 	platforms.forEach(platform => ctx.fillRect(platform.x, platform.y, platform.width, platform.height));
 }
 
+const renderDodgers = () => {
+	ctx.fillStyle = "#000000";
+	dodgers.forEach(dodger => ctx.fillRect(dodger.x, dodger.y, dodger.width, dodger.height));
+}
+
+const dodgerController = (dodgerElem) => {
+	if(dodgerElem.y > dodgerElem.origin_y) {
+		dodgerElem.velocity_y -= dodgerSpeed
+	} else {
+		dodgerElem.velocity_y += dodgerSpeed
+	};
+
+	dodgerElem.y += dodgerElem.velocity_y;
+};
+
+
+// Fireworks Settings ///////////////////////////////////////////////
+
+const numFireworks = 25;
+const fireworks = [];
+
 const renderFireworks = () => {
 	fireworks.forEach(firework => {
 		ctx.fillStyle = firework.color;
 		ctx.fillRect(firework.x, firework.y, firework.width,firework.height);
 	});
 };
-
-const goalParams = {};
-
-const renderGoal = (finalPlatformY) => {
-	goalParams.top = finalPlatformY - 130;
-	goalParams.bottom = finalPlatformY - 50;
-	ctx.fillStyle = "#80e080";
-	ctx.fillRect(490, (finalPlatformY - 50), 10, 15);
-	ctx.fillRect(490, (finalPlatformY - 130), 10, 15);
-}
-
-const renderDodgers = () => {
-	ctx.fillStyle = "#000000";
-	dodgers.forEach(dodger => ctx.fillRect(dodger.x, dodger.y, dodger.width, dodger.height));
-}
-
-const numFireworks = 25;
-const fireworks = [];
 
 const fireworkColorGenerator = () => {
 	const figures = '0123456789ABCDEF';
@@ -169,6 +201,21 @@ const createFireworks = () => {
 	};
 };
 
+
+// Goal Settings ///////////////////////////////////////////////////////
+
+const goalParams = {};
+
+const renderGoal = (finalPlatformY) => {
+	goalParams.top = finalPlatformY - 130;
+	goalParams.bottom = finalPlatformY - 50;
+	ctx.fillStyle = "#80e080";
+	ctx.fillRect(490, (finalPlatformY - 50), 10, 15);
+	ctx.fillRect(490, (finalPlatformY - 130), 10, 15);
+}
+
+// Level Generator Settings //////////////////////////////////////////////
+
 const generateNewLevel = () => {
 	platforms = [];
 	dodgers = [];
@@ -178,12 +225,16 @@ const generateNewLevel = () => {
 	createPlatforms();
 };
 
+//Win Settings ////////////////////////////////////////////////////////////
+
 const winCheck = () => {
 	if (player.x > canvasParams.width + 20 && player.y > goalParams.top - 30 && player.y < goalParams.bottom + 30) {
 	createFireworks();
 	generateNewLevel()
 	}
 };
+
+// Game Loop ///////////////////////////////////////////////////////////////
 
 const gameLoop = () => {
 	if(arrowKeys.right) {
@@ -208,15 +259,6 @@ const gameLoop = () => {
 	player.y += player.velocity_y;
 	player.x += player.velocity_x;
 
-	const dodgerController = (dodgerElem) => {
-		if(dodgerElem.y > dodgerElem.origin_y) {
-			dodgerElem.velocity_y -= dodgerSpeed
-		} else {
-			dodgerElem.velocity_y += dodgerSpeed
-		};
-
-		dodgerElem.y += dodgerElem.velocity_y;
-	};
 
 	dodgers.forEach(dodgerController);
 	
@@ -257,9 +299,7 @@ const gameLoop = () => {
 		}
 	};
 
-	fallCheck();
 
-	winCheck();
 
 	const fireworksController = fireworkElem => {
 		fireworkElem.velocity_y += gravity;
@@ -270,27 +310,15 @@ const gameLoop = () => {
 
 	fireworks.forEach(fireworksController);
 
+	fallCheck();
+	winCheck();
 	renderCanvas();
 	renderPlayer();
 	renderPlatforms();
 	renderDodgers();
 	renderGoal(currentPlatformY);
 	renderFireworks();
-
 } 
-
-const buttonTextFlash = (buttonName) => {
-	buttonName.style.color = "#E0E0E0";
-	buttonName.style.transition = "color 0.2s linear";
-	
-	setTimeout(() => {
-		buttonName.style.color = "unset";
-		buttonName.style.transition = "color 0.2s linear";
-	}, 300);
-};
-
-const retryButton = document.querySelector("#retry");
-const newLevelButton = document.querySelector("#new-level");
 
 canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
@@ -312,17 +340,7 @@ setInterval(gameLoop, 20);
 
 /*
 TODO:
-0. Check that canvas elements, esp. fireworks, are clearing. MAy eb sensible to add a setTimeout to clear the fireworks after 5s.
-1 - DONE - Improve the retry button.
-2. -DONE- Make a collision detector for the dodgers.
-3. -DONE- Program the result of the dodger collison detecor. Suggest returning the player to the starting position. 
-4. -DONE- Add a goal. I suggest a green or light blue marker at the right of the screen a set height above the final platform; the player jumps through this to 'win'.
-5. -DONE- Add a detector for falling off the level, and a fall condition.
-5. -DONE- Add a detector for 'win'. It will be the player passing through the goal.
-6. -DONE-Add a win animation! Can we do fireworks? I bet we can! Also atext 'you win!' in pixel text, and a 'try again? button which uses an event listener.
-6B. TODO: Update the firework controller so that rather than dispersing fireworks while theplayer is in the in area, it instead launches a set number of particles 'simultaneously' when the player enters the win area. To rephrase, alter the win condition detector to activate once when the player enters the area then not activate again for a short time. Also make it return the player to the start position, or erase the player.
-7. Add a counter for the number of wins.
-8. Add a nice animation for fail events eg. dodger collisions  falls.
-9. Refactor! In particular, move a lot of functions out of the game loop! Also sort them into a sensible order.
-10. Do we want to move different functions into different JS documents? Good modular practice...
+1. Add visible controls to the game screen. Could be a simnplelist of controls with instructions (probably best).
+2. Also state the goal. Jump between the green markers to win!
+3. Media Queries. Make the game work on all device sizes. Will we need to add mobile controls? Yes. How? Work it out.
 */
